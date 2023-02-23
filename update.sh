d138ebf7c7d5ef7d33a50f4cacaa8ee992eb62eb
CPAN Change Tracker <stig+cpan-change-tracker@stig.io>
2023-02-23 20:20:52 +0100

    Fix commit message

--- update.sh
+++ update.sh
@@ -8,5 +8,5 @@ START=$(date -Iseconds)
 ./mirror-cpan.sh
 ./update-hashes.sh
 STOP=$(date -Iseconds)
-git commit -m "Updated sha256sums $START_DATE\n\nstart=$START\nstop=$STOP" sha256sums
+git commit -m "Updated sha256sums $START_DATE (start=$START stop=$STOP)" sha256sums
 git push
